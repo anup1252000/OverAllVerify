48254fdd63f5a4ea9ef03925c82553a1659e8f3e
sruthi45 <sirekollam.sruthi18@gmail.com>
2023-07-23 15:11:34 +0530

    BankAccount Test projects

---
+++ BankAccount.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BankAccount.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ BankAccount.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BankAccount.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
